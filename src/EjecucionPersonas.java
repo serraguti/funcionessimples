fd2fba3b9ee4bf150f93bb099167d409e9dbb59d
serraguti <pacoserranox@gmail.com>
2025-01-13 20:40:27 +0100

    dsffsdf

--- src/EjecucionPersonas.java
+++ src/EjecucionPersonas.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class EjecucionPersonas {
     public static void main(String[] args) {
-        //12345678 -- Z
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduzca número de NIF");
         String entrada = teclado.nextLine();
@@ -10,5 +9,11 @@ public class EjecucionPersonas {
         int numeroDni = Integer.parseInt(entrada);
         String letra = HerramientasPersonas.getLetraNIF(numeroDni);
         System.out.println("Su letra de NIF es " + letra);
+        System.out.println("Introduzca un DNI completo");
+        String dni = teclado.nextLine();
+        boolean respuesta = 
+            HerramientasPersonas.validarNIF(dni);
+        System.out.println("El dni " + dni 
+        + " es correcto: " + respuesta);
     }
 }
--- src/HerramientasPersonas.java
+++ src/HerramientasPersonas.java
@@ -7,4 +7,23 @@ public class HerramientasPersonas {
             String.valueOf(letra);
         return letraConvertida;
     }
+
+    //QUISIERA UN METODO QUE RECIBA UN NIF/DNI
+    //Y NOS DEVUELVA SI ES VALIDO O NO
+    public static boolean validarNIF(String nif){
+        int longitud = nif.length() - 1;
+        //RECUPERAR EL ULTIMO CARACTER
+        char ultimaLetra = nif.charAt(longitud);
+        //ELIMINAMOS EL ULTIMO CARACTER
+        String soloNumeros = 
+            nif.substring(0, longitud);
+        //NECESITAMOS CONVERTIR LOS TIPOS
+        int numeroDni = Integer.parseInt(soloNumeros);
+        //CONVERTIMOS LA LETRA A STRING PARA COMPARAR
+        String letraComparacion = 
+            String.valueOf(ultimaLetra);
+        //LLAMAMOS AL METODO QUE NOS DEVUELVE UNA LETRA
+        String resultado = getLetraNIF(numeroDni);
+        return resultado.equals(letraComparacion);
+    }
 }
