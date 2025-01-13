75278e7956705bd0ef44c0cf7f904356715c7413
serraguti <pacoserranox@gmail.com>
2025-01-13 19:15:39 +0100

    aaasdadas

---
+++ src/EjecucionNumeros.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EjecucionNumeros {
+    public static void main(String[] args) {
+        //CREAMOS EL OBJETO TECLADO
+        Scanner teclado = new Scanner(System.in);
+        //MENSAJES PARA PEDIR DATOS
+        System.out.println("Introduzca el número 1");
+        //CAPTURAMOS LO QUE EL USUARIO HA ESCRITO
+        String entrada = teclado.nextLine();
+        //CAPTURAMOS EL PRIMER NUMERO
+        int numero1 = Integer.parseInt(entrada);
+        System.out.println("Introduzca número 2");
+        entrada = teclado.nextLine();
+        int numero2 = Integer.parseInt(entrada);
+        int mayor = EvaluacionNumeros.mayorDosNumeros(numero1, numero2);
+        int menor = EvaluacionNumeros.menorDosNumeros(numero1, numero2);
+        System.out.println("Mayor: " + mayor);
+        System.out.println("Menor: " + menor);
+    }
+}
---
+++ src/EvaluacionNumeros.java
@@ -0,0 +1,17 @@
+public class EvaluacionNumeros {
+    public static int mayorDosNumeros(int num1, int num2){
+        if (num1 > num2){
+            return num1;
+        }else{
+            return num2;
+        }
+    }
+    
+    public static int menorDosNumeros(int num1, int num2){
+        if (num1 < num2) {
+            return num1;
+        }else{
+            return num2;
+        }
+    }
+}
