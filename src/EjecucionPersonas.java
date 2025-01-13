1007e82247af136a4a1f0e140e47c1af20546de8
serraguti <pacoserranox@gmail.com>
2025-01-13 20:07:48 +0100

    dsadasd

--- src/EjecucionPersonas.java
+++ src/EjecucionPersonas.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class EjecucionPersonas {
     public static void main(String[] args) {
-        
+        //12345678 -- Z
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca número de NIF");
+        String entrada = teclado.nextLine();
+        //RECUPERAMOS EL NUMERO DE DNI
+        int numeroDni = Integer.parseInt(entrada);
+        String letra = HerramientasPersonas.getLetraNIF(numeroDni);
+        System.out.println("Su letra de NIF es " + letra);
     }
 }
